8217cbadd70c061528e4e22d62e8aa995db3450d
koichiro-kamide <tagayakk@gmail.com>
2026-01-04 20:37:15 +0900

    Reset src

--- src/parser/model.py
+++
@@ -1,7 +0,0 @@
-def add_model_options(parser):
-    group = parser.add_argument_group("Model options")
-    group.add_argument("--num_support", type=int, default=3, help="Number of samples in the support set")
-    group.add_argument("--num_augment", type=int, default=0, help="Number of augmented samples per support sample")
-    group.add_argument("--t_his", type=int, default=30, help="Number of observed frames")
-    group.add_argument("--t_pred", type=int, default=30, help="Number of predicted frames")
-    group.add_argument("--n_pred", type=int, default=20, help="Number of DCT bases used for prediction")
--- src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.utils.logger import *
-from src.utils.misc import *
-from src.utils.torch import *
-from src.utils.util import *
