3c2431ad3dec6cab1f8450f70117821d0310da31
koichiro-kamide <tagayakk@gmail.com>
2026-01-04 17:59:33 +0900

    Update prepare.

--- create_env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-mkdir -p data checkpoints_paper
-conda create -n haad python=3.8
-conda activate haad
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.1 -c pytorch
-pip install -r requirements.txt
