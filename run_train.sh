0dc13f1beb8a7e3b8ec2bd3c47ce946c878b78d7
koichiro-kamide <tagayakk@gmail.com>
2026-01-05 02:39:05 +0900

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -3,7 +3,7 @@
 python -m src.framework.train \
     --run_mode train \
     --cuda_index 0 \
-    --num_augment 2 \
+    --num_augment 1 \
     --batch_size 18 \
     --num_epochs 101 \
     --lr 0.001 \
